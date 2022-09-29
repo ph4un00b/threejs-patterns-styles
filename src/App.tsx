9950741991e68d341d48dae634616f997ab07ae0
ph4un00b <1057021+ph4un00b@users.noreply.github.com>
2022-09-29 16:20:24 -0500

    init shaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import Text from '../Text';
 import AntiGalactus from '../trips/AntiGalactus';
 import Casita from '../trips/Casita';
 import Drag from '../trips/Drag';
 import Fondos from '../trips/Fondos';
 import Noise from '../trips/Noise';
 import './App.css';
-import Lights from './Lights';
-import Materials from './Materials';
-import Models from './Models';
-import Particles from './Particles';
-import Physics from './Physics';
-import Raycaster from './Raycaster';
-import Shadows from './Shadows';
-import Textures from './Textures';
-import Three from './Three';
+import Text from '../basics/Text';
+import Lights from '../basics/Lights';
+import Materials from '../basics/Materials';
+import Models from '../basics/Models';
+import Particles from '../basics/Particles';
+import Physics from '../basics/Physics';
+import Raycaster from '../basics/Raycaster';
+import Shadows from '../basics/Shadows';
+import Textures from '../basics/Textures';
+import Three from '../basics/Three';
+import InitShader from '../shaders/InitShader';
 
 function App() {
-  return <Models />;
+  return <InitShader />;
+  // return <Models />;
   // return <Noise />;
   // return <Physics />;
   // return <Raycaster />;
