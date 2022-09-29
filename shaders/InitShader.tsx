00ec7cd77e2bcd6a85854c74ddf968cbdc68c699
ph4un00b <1057021+ph4un00b@users.noreply.github.com>
2022-09-29 16:31:58 -0500

    base shader

--- shaders/InitShader.tsx
+++ shaders/InitShader.tsx
@@ -64,11 +64,13 @@ export default function () {
             backgroundColor: fondo,
           }}
         >
+          {/* 💡 not having a clear color would glitch the recording */}
+          <color attach="background" args={['#000']} />
           {/* <React.Suspense> */}
 
           <PerspectiveCamera
             ref={cam_}
-            position={[-6, 6, 6]}
+            position={[-1, 1, 7]}
             fov={75}
             near={0.1}
             far={100}
@@ -78,222 +80,25 @@ export default function () {
           />
 
           <OrbitControls enableDamping={true} makeDefault={true} />
+          <group position={[0, 4, 0]}>
+            <Center>
+              <Text3D castShadow={true} font={global.font1}>
+                phau!
+                <meshStandardMaterial metalness={0} roughness={0} />
+              </Text3D>
+            </Center>
+          </group>
 
-          {/* @link https://pmndrs.github.io/cannon-es/docs/ */}
-          <Physics
-            /**
-             * - SAPBroadphase : ideal (performance)
-             * if your objects are not traveling too fast
-             * - NaiveBroadphase
-             *  */
-            broadphase={'SAP'}
-            /** does not update quiet objects */
-            allowSleep={true}
-          >
-            <Debug color="black" scale={1.1}>
-              <group position={[0, 4, 0]}>
-                <Center>
-                  <Text3D castShadow={true} font={global.font1}>
-                    phau!
-                    <meshStandardMaterial metalness={0} roughness={0} />
-                  </Text3D>
-                </Center>
-              </group>
-
-              <Cubo castShadow={true} />
-              <World items={4} />
-            </Debug>
-          </Physics>
-
+          <mesh>
+            <planeBufferGeometry args={[1, 1, 32, 32]} />
+            <rawShaderMaterial vertexShader={''} fragmentShader={''} />
+          </mesh>
           <axesHelper args={[4]} />
           <ambientLight color={ambient} intensity={ambientIntensity} />
-          <DirectionalLight
-            castShadow={true}
-            position={[5, 5, 5]}
-            intensity={0.2}
-            color={ambient}
-          />
           {/* </React.Suspense> */}
         </Canvas>
       </section>
     </>
-  );
-}
-
-var hitSound = new window.Audio(`${baseUrl}/sounds/hit.mp3`);
-
-function playHit({ contact: { impactVelocity } }: CollideEvent) {
-  if (impactVelocity < 1.5) return;
-  /** todo:
-   * - change the  volume in relation to impact
-   * - add a little delay since hitting the floor
-   * fires multiple events (4?)
-   */
-  hitSound.volume = Math.random();
-  hitSound.currentTime = 0;
-  hitSound.play();
-}
-
-type MaterialOptions =
-  | string
-  | {
-      friction?: number | undefined;
-      restitution?: number | undefined;
-    }
-  | undefined;
-
-function World({ items }: { items: number }) {
-  const [circulos, setCirculos] = React.useState(items);
-  const [preset] = React.useState(Math.floor(Math.random() * 900));
-
-  const bouncyMat = {
-    name: 'plastic',
-  } as MaterialOptions;
-
-  const groundMat = {
-    name: 'concreto',
-  } as MaterialOptions;
-
-  useContactMaterial(groundMat, bouncyMat, {
-    friction: 0.1,
-    restitution: 0.7 /** bouncing! */,
-  });
-
-  const colors = React.useMemo(() => {
-    const array = new Float32Array(items * 3);
-    const color = new T.Color();
-    for (let i = 0; i < items; i++) {
-      color
-        .set(nice_colors[preset][Math.floor(Math.random() * 4)])
-        .convertSRGBToLinear()
-        .toArray(array, i * 3);
-    }
-    return array;
-  }, [items]);
-
-  return (
-    <>
-      {/* using position instead of granular ones 'position-x' */}
-      <Esfera castShadow={true} material={bouncyMat} position={[-2, 5, 0]} />
-
-      {/* using rotation instead of granular ones 'rotation-x' */}
-      <Piso
-        receiveShadow={true}
-        material={groundMat}
-        rotation={[-Math.PI * 0.5, 0, 0]}
-      />
-    </>
-  );
-}
-function Piso({
-  receiveShadow,
-  ...props
-}: PlaneProps & { receiveShadow?: boolean }) {
-  // infinite grid floor
-  const [piso, api] = usePlane(
-    () => ({ mass: 0, ...props }),
-    React.useRef<T.Mesh>(null!)
-  );
-
-  return (
-    <mesh ref={piso} receiveShadow>
-      <planeBufferGeometry args={[16, 16]} />
-      <meshStandardMaterial
-        color={'#777777'}
-        roughness={0.4}
-        metalness={0.3}
-        side={T.DoubleSide}
-      />
-    </mesh>
-  );
-}
-
-function Esfera({
-  receiveShadow,
-  castShadow,
-  ...props
-}: SphereProps & { receiveShadow?: boolean; castShadow?: boolean }) {
-  const [esfera, world] = useSphere(
-    () => ({
-      mass: 1,
-      onCollide: (e) => {
-        playHit(e);
-      },
-      ...props,
-    }),
-    React.useRef<T.Mesh>(null!)
-  );
-
-  React.useEffect(() => {
-    world.applyLocalForce([150, 0, 0], [0, 0, 0]);
-  }, []);
-
-  useFrame(({ clock }) => {
-    // api.position.set(Math.sin(clock.getElapsedTime()) * 5, 1, 0)
-    world.applyForce([-0.05, 0, 0], esfera.current!.position.toArray());
-  });
-
-  return (
-    // shall we spread the props again?
-    <mesh ref={esfera} castShadow>
-      <sphereBufferGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial roughness={0.4} metalness={0.3} />
-    </mesh>
-  );
-}
-
-function Cubo(props: BoxProps & MeshProps) {
-  const [active, setActive] = React.useState(0);
-  const cubo = React.useRef<T.Mesh>(null);
-  const { pos } = useSpring({
-    to: {
-      pos: 0,
-    },
-    from: { pos: -20 },
-    config: config.gentle,
-  });
-  const { scale } = useSpring({ scale: active ? 4 : 1 });
-  const { rotation } = useSpring({ rotation: active ? Math.PI : 0 });
-  const { colorA } = useSpring({ colorA: active ? 'royalblue' : '#e83abf' });
-  /** interpolate values from common spring */
-  // const { spring } = useSpring({
-  //   spring: active,
-  //   config: config.molasses,
-  // });
-  // const { pos } = useSpring({ pos: active ? -2 : 0 });
-  // const scale = spring.to([0, 1], [1, 4]);
-  // const rotation = spring.to([0, 1], [0, Math.PI]);
-  // const colorA = spring.to([0, 1], ['#6246ea', 'royalblue']);
-
-  const { viewport } = useThree();
-  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 1 }));
-
-  const handlers = useDrag(function ({ event, offset: [x, y] }) {
-    event.stopPropagation();
-    const aspect = viewport.getCurrentViewport().factor;
-    console.log(x, y);
-    return api.start({ x: x / aspect, y: -y / aspect });
-  });
-
-  return (
-    <a.mesh
-      {...handlers()}
-      {...props}
-      onClick={(e) => {
-        e.stopPropagation();
-        setActive(Number(!active));
-      }}
-      rotation-x={rotation}
-      scale={scale}
-      position-x={x}
-      position-y={y}
-      position-z={pos}
-    >
-      <boxGeometry />
-      {/* 
-      // @ts-ignore */}
-      <a.meshStandardMaterial color={colorA} />
-    </a.mesh>
   );
 }
 
