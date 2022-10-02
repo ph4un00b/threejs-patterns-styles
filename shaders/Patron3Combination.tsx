9e864ca26d20f111a8ee34126947ce4c0aeccab4
ph4un00b <1057021+ph4un00b@users.noreply.github.com>
2022-10-02 16:28:10 -0500

    combination by max

--- shaders/Patron3Combination.tsx
+++ shaders/Patron3Combination.tsx
@@ -485,8 +485,30 @@ varying vec2 vUv;
 varying float vElevation;
 
 void main() {
-  // range: [0.5, 0.5]
   float combo = min( abs(vUv.x - 0.5), abs(vUv.y - 0.5) );
+
+  // black 0,0,0 ,  white 1,1,1
+  gl_FragColor = vec4(combo, combo, combo, 1.0);
+
+}
+`;
+
+var frag = glsl`
+/** context -> inputs */
+
+uniform float utime;
+uniform float uleverX;
+uniform float uleverY;
+uniform float uleverA;
+uniform float uleverB;
+
+/** vertex -> inputs */
+
+varying vec2 vUv;
+varying float vElevation;
+
+void main() {
+  float combo = max( abs(vUv.x - 0.5), abs(vUv.y - 0.5) );
 
   // black 0,0,0 ,  white 1,1,1
   gl_FragColor = vec4(combo, combo, combo, 1.0);
