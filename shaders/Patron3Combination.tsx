5e9413c9537a29c0dc1587666363ffa2bb992310
ph4un00b <1057021+ph4un00b@users.noreply.github.com>
2022-10-02 16:18:50 -0500

    tweak

--- shaders/Patron3Combination.tsx
+++ shaders/Patron3Combination.tsx
@@ -457,12 +457,11 @@ varying vec2 vUv;
 varying float vElevation;
 
 void main() {
+  // range: [-0.5, 0.5]
+  // float combo = vUv.x - 0.5;
 
-  /** range [0.0, 1.0]
-  // float combo = vUv.x;
-
-  /** range [0.5, 0.5]
-   float combo = abs(vUv.x - 0.5);
+  // range: [0.5, 0.5]
+  float combo = abs(vUv.x - 0.5);
 
   // black 0,0,0 ,  white 1,1,1
   gl_FragColor = vec4(combo, combo, combo, 1.0);
